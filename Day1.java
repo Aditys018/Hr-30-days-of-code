0e43eb2507b7fefff551cd44c1f08c8facb23208
Aditi Shinde <100122761+Aditys018@users.noreply.github.com>
2023-06-05 05:30:13 +0530

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int a = 4 ;
+        double b = 4.0;
+        String c = "HackerRank ";
+        
+        Scanner scan = new Scanner(System.in);
+        int d = scan.nextInt();
+        double e = scan.nextDouble();
+         scan.nextLine();
+        String f = scan.nextLine();
+        
+        System.out.println(a+d);
+        System.out.println(b+e);
+        System.out.println(c + f);
+        
+        scan.close();
+    }
+}
